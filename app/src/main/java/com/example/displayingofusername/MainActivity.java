1c448adf4a74e4c634909b89c2dc07e15312a9df
johnkrupakar <john245461@gmail.com>
2022-05-07 20:55:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/displayingofusername/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.displayingofusername;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editText);
+        editText.setTextColor(Color.WHITE);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                Toast.makeText(getApplicationContext(), "User Name is :"+name, Toast.LENGTH_SHORT).show();
+                Log.v("test",""+name);
+            }
+        });
+    }
+}
